258325e5ca9cd3029215c86bdcf0f2fa44c5f1f6
Ahmet Özışık <ozisikahmet@gmail.com>
2019-03-16 19:22:54 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,86 @@
+Vue Bootstrap 4 Modal
+=====================================
+
+This is an opinionated bootstrap 4 modal component. Use this if you want to quickly open a Bootstrap 4 modal, implement a custom handler for the primary action button or listen to show / hide events.
+
+
+### Add to Your Project
+
+Will be added to npm registry soon. Until then,
+
+```bash
+yarn add https://github.com/swiftmade/vue-bs4-modal
+```
+
+Then register the component in your Vue app:
+
+```js
+Vue.component('bs4-modal', require('vue-bs4-modal'));
+```
+
+
+#### Caveats 
+
+* Make sure jQuery is globally accessible via `$`.
+* You must include Bootstrap 4 script and stylesheet in your project.
+
+### Properties
+
+| Prop        | Type                | Description                                                                                                                                                         |
+|-------------|---------------------|---------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| handler     | Function -> Promise | The handler will be called when the action button is pressed. You MUST return a promise from your handler. UI will be in busy state until your promise is resolved. |
+| actionLabel | String              | Label of your action button. By default, it will read "OK"                                                                                                          |
+| cancelLabel | String              | Label of your cancel button. By default, it will read "Cancel"                                                                                                      |
+| isLarge     | Boolean             | Pass true to make your modal large (`modal-lg`). Otherwise, `modal-default` will be used.                                                                           |
+
+### Example Usage
+
+In the following example, pressing "Open Modal" will open 
+
+```vue
+<template>
+	<div>
+		<bs4-modal ref="modal" :handler="modalHandler">
+          <div slot="header">Your Modal's Title</div>
+          <div>
+          	Your Modal's body! Pressing OK will make you wait 5 seconds...
+          </div>
+		</bs4-modal>
+		
+		<button @click="openModal()">Open Modal</button>
+	</div>
+</template>
+
+<script>
+export default {
+
+	methods: {
+
+		openModal() {
+			this.$refs.modal.show()
+				.then(() => {
+					console.log('Modal is shown!');
+					return this.$refs.modal.waitForResponse();
+				})
+				.then(
+					() => {
+						console.log('Modal action is completed!');
+					},
+					() => {
+						console.log('Modal is cancelled !');
+					}
+				);
+		},
+		
+		modalHandler() {
+			return new Promise(resolve => {
+				setTimeout(resolve, 5000);
+			});
+		},
+
+	}
+}
+</script>
+```
+
+
---
+++ src/Bs4Modal.vue
@@ -0,0 +1,127 @@
+<template>
+  <div v-if="shown">
+    <div class="modal fade" ref="modalEl">
+      <div class="modal-dialog" :class="modalClass">
+        <div class="modal-content">
+          <div class="modal-header">
+            <slot name="header"></slot>
+          </div>
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+          <div class="modal-footer">
+            <slot name="footer">
+              <div class="text-right">
+                  <button type="button" class="btn btn-light border mr-3" @click="cancel()">{{ cancelLabel }}</button>
+                  <button type="button" class="btn btn-primary border" @click="_process()" :disabled="busy">{{ actionLabel }}</button>
+              </div>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>        
+  </div>
+</template>
+<script>
+export default {
+
+  props: {
+    isLarge: {type: Boolean, default: false},
+    cancelLabel: {type: String, default: 'Cancel'},
+    actionLabel: {type: String, default: 'OK'},
+    handler: {type: Function, default: () => Promise.resolve(true)},
+  },
+
+  data() {
+    return {
+      busy: false,
+      shown: false,
+      onSuccess: null,
+      onCancel: null,
+      modalClass: this.isLarge ? 'modal-lg' : 'modal-default',
+    };
+  },
+
+  methods: {
+
+    /**
+     * Shows the modal.
+     * Returns a promise that resolves when the modal is shown.
+     */
+    show() {
+      this.shown = true;
+
+      return new Promise((resolve) => {
+        Vue.nextTick(() => {
+          $(this.$refs.modalEl).one('show.bs.modal', () => resolve());
+          $(this.$refs.modalEl).one('hide.bs.modal', () => this._onModalClosed());
+          $(this.$refs.modalEl).modal('show');
+        });
+      });
+    },
+
+    /**
+     * Cancel the modal prematurely. Closes the modal.
+     * If waitForResponse was called, it will be resolved with a rejection.
+     */
+    cancel() {
+        this.busy = false;
+        this._closeModal();
+    },    
+
+    /**
+     * Waits until the action in the modal is finished or cancelled.
+     * Pressing the action button will call the "handler" prop that you passed.
+     * If it returns a value, the promise will be resolved with this value.
+     * If it returns a promise, it will wait in busy state until the promise is resolved, then return its value.
+     * If cancel is pressed or the modal is closed by pressing escape, you will get a rejection.
+     */
+    waitForResponse() {
+      return new Promise((resolve, reject) => {
+          this.onCancel = reject
+          this.onSuccess = resolve
+      });
+    },
+
+    _onModalClosed() {
+      this.shown = false;
+      this._failPromise('Cancelled');
+    },
+
+    _finishPromise(data) {
+        if(this.onSuccess) {
+            this.onSuccess(data)
+            this._clearHandlers()
+        }
+    },
+    
+    _failPromise(msg) {
+        if (this.onCancel) {
+            this.onCancel(new Error(msg))
+            this._clearHandlers()
+        }
+    },
+    
+    _clearHandlers() {
+        this.onCancel = null
+        this.onSuccess = null  
+    },    
+
+    _closeModal() {
+        $(this.$refs.modalEl).modal('hide');
+    },
+
+    _process() {
+        this.busy = true
+        this.handler().then((response) => {
+            this.busy = false
+            this._finishPromise(response)
+            this._closeModal()
+        }).catch((error) => {
+            this.busy = false
+            // ERROR...
+        })
+    }
+  },
+}
+</script>
